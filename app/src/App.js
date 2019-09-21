189e5bf1496a9644b79c1ad381a10fb280774a2f
John Collins <j.k.collins410@gmail.com>
2019-09-21 16:26:39 -0400

    added unauthorized access attempt handling with Forbidden route

--- app/src/App.js
+++ app/src/App.js
@@ -14,10 +14,11 @@ import UserSignOut from './components/UserSignOut';
 import Authenticated from './components/Authenticated';
 import CourseDetail from './components/CourseDetail';
 import CreateCourse from './components/CreateCourse';
-
+import Forbidden from "./components/Forbidden";
 
 import withContext from './Context';
 import PrivateRoute from './PrivateRoute';
+
 
 const HeaderWithContext = withContext(Header);
 const CoursesWithContext = withContext(Courses);
@@ -27,6 +28,7 @@ const UserSignInWithContext = withContext(UserSignIn);
 const UserSignOutWithContext = withContext(UserSignOut);
 const CourseDetailWithContext = withContext(CourseDetail);
 const CreateCourseWithContext = withContext(CreateCourse);
+const ForbiddenWithContext = withContext(Forbidden);
 
 export default () => (
     <Router>
@@ -36,12 +38,13 @@ export default () => (
         <Switch>
           <Route exact path="/" component={CoursesWithContext}/>
           <PrivateRoute path="/authenticated" component={AuthWithContext}/>
+          <PrivateRoute path="/courses/new" component={CreateCourseWithContext}/>
           <Route path="/signin" component={UserSignInWithContext}/>
           <Route path="/signup" component={UserSignUpWithContext}/>
           <Route path="/signout" component={UserSignOutWithContext}/>
           <Route exact path="/courses" component={CoursesWithContext}/>
           <Route path="/courses/:id" component={CourseDetailWithContext}/>
-          <Route path="/courses/new" component={CreateCourseWithContext}/>
+          <Route path="/forbidden" component={ForbiddenWithContext}/>
           <Route component={NotFound}/>
         </Switch>
       </div>
