2d3c11c57d0d2d1049f399053b937470ddf7bfa9
John Collins <j.k.collins410@gmail.com>
2019-09-21 16:04:45 -0400

    File added to handle unauthorized access attempts

---
+++ app/src/components/Forbidden.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+  return (
+      <div className="bounds">
+        <div className="grid-100">
+          <h1>Forbidden</h1>
+          <h3>I'm sorry, you do not have authorization to view this page</h3>
+        </div>
+      </div>
+  );
+}
