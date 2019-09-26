8074aa9b1eed5cea532b0fed2a063dc3dfcfee52
John Collins <j.k.collins410@gmail.com>
2019-09-26 13:27:46 -0400

    Added updateCourse function

--- client/src/Data.js
+++ client/src/Data.js
@@ -14,6 +14,7 @@ export default class Data {
     if (body !== null) {
       options.body = JSON.stringify(body);
     }
+
     if (requiresAuth) {
       const encodedCredentials = btoa(`${credentials.credentials.emailAddress}:${credentials.credentials.password}`);
       options.headers['Authorization'] = `Basic ${encodedCredentials}`;
@@ -23,7 +24,7 @@ export default class Data {
   }
 
   async getUser(emailAddress, password) {
-    const response = await this.api(`/users`, 'GET', null, true, {emailAddress, password});
+    const response = await this.api(`/users`, 'GET', null, true, {credentials: {emailAddress, password}});
     if (response.status === 200) {
       return response.json().then(data => data);
     } else if (response.status === 401) {
@@ -34,7 +35,16 @@ export default class Data {
   }
 
   async createUser(user) {
-    const response = await this.api('/users', 'POST', user);
+    const {emailAddress} = user;
+    const {password} = user;
+    const {firstName} = user;
+    const {lastName} = user;
+    const response = await this.api('/users', 'POST', {
+      emailAddress,
+      password,
+      firstName,
+      lastName
+    }, true, {credentials: {emailAddress, password}});
     if (response.status === 201) {
       return [];
     } else if (response.status === 400) {
@@ -60,6 +70,37 @@ export default class Data {
     }, true, {credentials: {emailAddress, password}});
     if (response.status === 201) {
       return console.log('Course created');
+    } else if (response.status === 400) {
+      return response.json().then(data => {
+        return data.errors;
+      });
+    } else {
+      throw new Error();
+    }
+  }
+
+  async updateCourse(course, {emailAddress, password, userId}) {
+    const {title} = course;
+    const {description} = course;
+    const {estimatedTime} = course;
+    const {materialsNeeded} = course;
+    const {id} = course;
+    const response = await this.api(`/courses/${id}`, 'PUT', {
+      title,
+      description,
+      estimatedTime,
+      materialsNeeded,
+      userId,
+      id
+    }, true, {credentials: {emailAddress, password}});
+    if (response.status === 204) {
+      console.log(this);
+      return console.log('Course updated');
+    } else if (response.status === 404) {
+      console.log(response);
+      return response.json().then(data => {
+        return data.errors;
+      });
     } else if (response.status === 400) {
       return response.json().then(data => {
         return data.errors;
