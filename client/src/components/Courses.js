8aeb6fc56df0038d95b94f337b38ee07cb862749
John Collins <j.k.collins410@gmail.com>
2019-09-26 20:34:12 -0400

    Route updated to match project requirements

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -26,7 +26,7 @@ export default class Courses extends Component {
           })
           }
           <div className="grid-33">
-            <Link className="course--module course--add--module" to={`/courses/new`}>
+            <Link className="course--module course--add--module" to={`/courses/create`}>
               <h3 className="course--add--title">
                 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13"
                      className="add">
