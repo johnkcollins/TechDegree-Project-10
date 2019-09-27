86a237f2effed9bbf2c9ff477f84220698615971
John Collins <j.k.collins410@gmail.com>
2019-09-27 10:04:59 -0400

    Added a transition page for deleting a course

---
+++ client/src/components/DeleteCourse.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+  return (
+      <div className="bounds">
+        <div className="grid-100">
+          <h1>Please wait deleting course....</h1>
+        </div>
+      </div>
+  );
+}
