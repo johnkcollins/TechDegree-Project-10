827c3c694d2cd8686d1e35e3c038cf3a10742405
John Collins <j.k.collins410@gmail.com>
2019-09-27 10:04:34 -0400

    Fixed a bug allowing course page to render without courses data in state

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -4,27 +4,32 @@ import {Link} from "react-router-dom";
 export default class Courses extends Component {
 
   state = {
-    courses: []
+    courses: null
   };
 
   componentDidMount() {
     this.getCourses();
   }
 
+
   render() {
+    let course;
+    if (this.state.courses) {
+      course = this.state.courses.map(course => {
+        return (
+            <div className="grid-33" key={course.id}>
+              <Link className="course--module course--link" to={`/courses/${course.id}`} key={course.id}>
+                <h4 className="course--label">Course</h4>
+                <h3 className="course--title">{course.title}</h3>
+              </Link>
+            </div>
+        )
+      });
+    }
+
     return (
         <div className="bounds">
-          {this.state.courses.map(course => {
-            return (
-                <div className="grid-33" key={course.id}>
-                  <Link className="course--module course--link" to={`/courses/${course.id}`} key={course.id}>
-                    <h4 className="course--label">Course</h4>
-                    <h3 className="course--title">{course.title}</h3>
-                  </Link>
-                </div>
-            )
-          })
-          }
+          {this.state.courses ? course : ''}
           <div className="grid-33">
             <Link className="course--module course--add--module" to={`/courses/create`}>
               <h3 className="course--add--title">
@@ -44,7 +49,9 @@ export default class Courses extends Component {
     const url = '/courses';
     const response = await this.props.context.data.api(url);
     if (response.status === 200) {
-      response.json().then(data => this.setState({courses: data}))
+      await response.json().then(data => this.setState({courses: data}));
+      console.log(response);
+      console.log(this.state.courses);
     } else if (response.status === 500) {
       this.props.history.push('/error');
     } else {
